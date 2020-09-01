68775df9c7ad255bdf7c86bac6a14961e6414b23
user <user@debian-10.development>
2020-09-01 14:24:34 -0500

    add basic authentication, about to start Securing New Users in I5 section of tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :tags
   resources :authors
 
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
